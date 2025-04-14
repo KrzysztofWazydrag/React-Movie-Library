a6bc3d751c7efeb902792349f30db8e53ad3316d
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2025-04-15 00:26:23 +0100

    style and logo edited

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,8 @@ import "../css/Navbar.css";
 function NavBar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+        <img  src={"/src/public/movie-app.png"} alt="Movie App Logo" className="app-logo" />
+            <Link to="/" style={{ color: '#e50914' }}>Movie App</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
