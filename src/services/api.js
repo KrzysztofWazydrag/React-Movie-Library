224b056dae573adc3f1549f2f74ed9e292362c27
Krzysztof Wazydrag <krzysztof.wazydrag@gmail.com>
2025-04-21 18:05:20 +0100

    comments added

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,13 @@ const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
 
 const BASE_URL = "https://api.themoviedb.org/3";
 
+// This function communicates with the API to get the details of a movie
 export const getPopularMovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
     const data = await response.json()
     return data.results
 };
-
+// This function allows to search for movies from API by a given query string.
 export const searchMovies = async (query) => {
     const response = await fetch(
         `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
